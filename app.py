7ec7dae7114c1d3bae5e3c987397b12f10d029fe
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-17 17:04:39 -0500

    ok works but needs to be capilized not sure if thats gonna matter right now

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ while app:
 	app_start = input("Play the state capitals game? (y to start)")
 	if app_start == "y":
 		print ("nice!  this is better than sporcle anyway")
-		answer = input(f"type the capital of {states[0]['name']}")
+		answer = input(f"type the capital of {states[0]['name']}: ")
+		if answer == states[0]["capital"]:
+			print("correct! you is smart, you is kind, you is important")
 
 
