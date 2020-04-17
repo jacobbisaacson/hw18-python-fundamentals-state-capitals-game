64fcc449fb41ed9ae9af783165f1fda9d338aa60
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-17 17:14:19 -0500

    hell yea

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ print('hw18')
 from capitals import states
 import random
 # print(states)
+for state in states:
+	state['correct'] = 0
+	state['incorrect'] = 0
 
 random.shuffle(states)
 # print(states)
@@ -11,8 +14,10 @@ while app:
 	app_start = input("Play the state capitals game? (y to start)")
 	if app_start == "y":
 		print ("nice!  this is better than sporcle anyway")
-		answer = input(f"type the capital of {states[0]['name']}: ")
-		if answer == states[0]["capital"]:
-			print("correct! you is smart, you is kind, you is important")
+		for state in states:
+			answer = input(f"type the capital of {state['name']}: ")
+			if answer == state["capital"]:
+				print("correct! you is smart, you is kind, you is important")
+
 
 
