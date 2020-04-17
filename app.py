7d64e7b6bcae285963975fd71c351fff35cfb350
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-17 17:34:45 -0500

    score adding

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ for state in states:
 
 random.shuffle(states)
 # print(states)
-
+app_score = 0
 app = True
 while app:
 	app_start = input("Play the state capitals game? (y to start)")
@@ -18,12 +18,13 @@ while app:
 			answer = input(f"type the capital of {state['name']}: ")
 			if answer == state["capital"]:
 				state['correct'] += 1
+				app_score += 1
 				print("correct! you is kind, you is smart, you is important")
-				print(f"you have gotten {state['correct']} right!")
+				print(f"you have gotten {app_score} right! Keep going until you get all 50!")
 			if answer != state["capital"]:
 				state['incorrect'] += 1
-				print("wrong!")
-				print(f"oooh sorry, you have gotten {state['incorrect']} wrong.")
+				print("oooh sorry, wrong!")
+
 
 
 
