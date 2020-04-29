5447ca662ac637a810db52f21bcc3a7c76944c03
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-29 17:56:22 -0500

    comitting before going back to refactor

--- app.py
+++ app.py
@@ -7,14 +7,23 @@ for state in states:
 	state['incorrect'] = 0
 
 random.shuffle(states)
-# print(states)
-# def 
 app_score = 0
 app = True
-while app:
+
+def start():
+# while app:
 	app_start = input("Play the state capitals game? (y to start)")
 	if app_start == "y":
 		print ("nice!  this is better than sporcle anyway")
+
+    # THIS IS WHERE TO ASK TIMM FOR HELP!!
+    
+    play()
+  else:
+    print("k bye")
+    sys.exit()
+
+def play():
 		for state in states:
 			answer = input(f"type the capital of {state['name']}: ")
 			if answer == state["capital"]:
@@ -22,23 +31,21 @@ while app:
 				app_score += 1
 				print("correct! you is kind, you is smart, you is important")
 				print(f"you have gotten {app_score} right! Keep going until you get all 50!")
+        # print(f"this has been guessed correct {state['correct']} times! \n this has been guessed incorrect {state['incorrect']} times!")
 			if answer != state["capital"]:
 				state['incorrect'] += 1
 				print("oooh sorry, wrong!")
 		print('you win!')
-		app_again = input(f"wanna play again? (y to restart)")
-		if app_again == 'y':
-			app = True
-		else:
-			app = False
-	else:
-		print("k fine bye!")
-		app = False
 
-		if play_again == "y":
-			app = True
-							
+def play_again():
+  play_again = input(f"wanna play again? (y to restart)")
+  if play_again == 'y':
+    app = True
+  else:
+    app = False
+    print("k fine bye!")
 
+start()
 
 
 
